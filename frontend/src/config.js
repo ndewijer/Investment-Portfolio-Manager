1ac2df4bb17ad3553edb945469c0b0a9ab0ff976
Nick de Wijer-Graafstra <nick@dewijer.nl>
2024-11-11 21:42:33 +0100

    https for backend in frontend config when going through reverse proxy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,7 @@ const IS_DEV = process.env.NODE_ENV === 'development';
 // In development, use localhost, in production use the domain
 const API_URL = IS_DEV 
   ? 'http://localhost:5000/api'
-  : `http://${DOMAIN}/api`;
+  : `https://${DOMAIN}/api`;
 
 export const API_BASE_URL = API_URL;
 
